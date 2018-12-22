02627bb4ea9e3cdca5b9f55acd42926b0046af33
Dom <dominicsmith1021@gmail.com>
2018-12-22 10:16:28 -0500

    Changed moving fog to canvas image

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,10 @@
+$(document).ready(function(){
+
 var myGameArea = {
-    fogger : false,
-    loopFog : function() {
-        if (this.fogger == false) {return;};
-        $("#background").animate({"right":"-184vh"},60000,"linear",function() {
-            $("#background").css("right","-552vh");
-            myGameArea.loopFog();
-        });
-    },
-    display : function(subject,index) {
+    canvas : $("canvas").get()[0],
+    fog : [new component(0, 0, "assets/images/fog.png", 0, 0, "image"),new component(0, 0, "assets/images/fog.png", 0, 0, "image")],
+    
+    display(subject,index) {
         const question = _G.myQuestions.questions[subject][index];
         $('#question').text(question.q);
         $('#choices').html(''); //Clearing answer choices
@@ -52,7 +49,19 @@ var myGameArea = {
             },2000);
         });
     },
+
+    timer() {
+
+    },
+
+    clear : function() {
+        if (!this.context) {return;};
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+
+    interval : setInterval(updateGameArea, 1000/50)
 };
+myGameArea.context = myGameArea.canvas.getContext("2d"); //Drawing tool
 
 var myGameStats = {
     incorrect: 0,
@@ -69,34 +78,89 @@ var myGameStats = {
     },
 };
 
+//Function for drawing on canvas
+function component(width, height, appearance, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = appearance;
+    };
+    this.width = width;
+    this.height = height;
+    this.left = 0;
+    this.right = 0;
+    this.up = 0;
+    this.down = 0;
+    this.x = x;
+    this.y = y;  
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (type == "image") {
+            ctx.drawImage(this.image,
+                this.x,
+                this.y,
+                this.width,
+                this.height);
+        };
+    };
+    this.newPos = function() {
+        if (this.x < -this.width+10) {
+            this.x = myGameArea.canvas.width-10;
+        };
+        this.x -= this.left;
+        this.y += (-this.up)+(-this.down); 
+        return     
+    };    
+};
+
+//Setting up default positions for fog
+var comp = getComputedStyle(document.body);
+var fogWidth = parseInt(comp.height.replace("px",""))*1.84;
+myGameArea.fog[0].x = -fogWidth/2;
+myGameArea.fog[1].x = fogWidth/2;
+fogWidth = null;
+
+//Update canvas frames
+var frame = 0;
+function updateGameArea() {
+    frame++;
+    comp = getComputedStyle(document.body);
+    var windowHeight = comp.height.replace("px","");
+    var windowWidth = comp.width.replace("px","");
+    myGameArea.canvas.height = parseInt(windowHeight);
+    myGameArea.canvas.width = parseInt(windowWidth);
+    myGameArea.clear();
+   
+    myGameArea.fog.forEach(function(fog) {
+        fog.left = 1;
+        fog.height = windowHeight;
+        fog.width = windowHeight*1.84;
+        fog.newPos();
+        myGameArea.context.globalAlpha = 0.5;
+        fog.update();
+        myGameArea.context.globalAlpha = 1;
+    });
+};
+
+
 myGameStats.reset();
-
-$(document).ready(function(){
-
 myGameArea.display("Chucky",0);
 
 $("#background").css("right","-552vh");
 
-myGameArea.loopFog();
-
+//Starting the game
 $("#play").on("click", function() {
     $("#intro-container").css("display","none");
     $("#game-container").css("display","block");
-    if (myGameArea.fogger == false) {
-        myGameArea.fogger = true;
-        myGameArea.loopFog();
-    };
-    $("#ambience").get()[0].play();
+    $("#ambience").get()[0].play(); //Playing background mmusic
 });
 
+//When the user comes back to the page
 $(window).focus(function() {
-    if (myGameArea.fogger == false) {
-        myGameArea.fogger = true;
-        myGameArea.loopFog();
-    };
     $("#ambience").get()[0].play();
 });
 
+//When the user leaves the page
 $(window).blur(function() {
     myGameArea.fogger = false;
     $("#ambience").get()[0].pause();
